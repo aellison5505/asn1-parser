f5e49aa4a1272706641b67d61a57d89cb02c8fb9
jbfd-ems <25670817+aellison5505@users.noreply.github.com>
2020-09-06 18:42:43 -0400

    docs

--- lib/asn1.d.ts
+++ lib/asn1.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="node" />
 import { TagBuilder } from './tagBuilders';
+/**
+ * This will decode and build ASN.1
+ * Currently supported tags are
+ * INTEGER
+   BIT_STRING
+   OCTET_STRING
+   OBJECT_IDENTIFIER
+   SEQUENCE
+ */
 export declare class ASN1 {
     decode(encoding: Buffer): string;
     build(tagBuilder: TagBuilder): string;
--- src/asn1.ts
+++ src/asn1.ts
@@ -1,7 +1,15 @@
 import { Decode } from './decode';
 import {TagBuilder } from './tagBuilders';
 
-
+/**
+ * This will decode and build ASN.1 
+ * Currently supported tags are 
+ * INTEGER
+   BIT_STRING
+   OCTET_STRING
+   OBJECT_IDENTIFIER
+   SEQUENCE
+ */
 export class ASN1 {
 
    decode(encoding: Buffer): string {
